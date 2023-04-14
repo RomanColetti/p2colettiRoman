6e04bd70e734753dfa958ec76f41149b87ed8563
RomanColetti <129554236+RomanColetti@users.noreply.github.com>
2023-04-14 19:30:04 -0300

    Add files via upload

---
+++ TP1/js/funciones.js
@@ -0,0 +1,8 @@
+console.log("funciona");
+
+function mifuncion(){
+    const apellido=document.getElementById("apellido");
+    const nombre=document.getElementById("nombre");
+    const edad=document.getElementById("edad")
+    alert(apellido.value +" "+ nombre.value +" "+ edad.value);
+}
